03a71ca3b41f71ad7d9ff9613c601f3634d8f624
Sai krishna <saaiiikrishna@gmail.com>
2025-06-17 04:50:35 +0530

    Initial commit

---
+++ backend/src/main/java/com/dwelzy/dwelzy/DwelzyApplication.java
@@ -0,0 +1,13 @@
+package com.dwelzy.dwelzy;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DwelzyApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DwelzyApplication.class, args);
+	}
+
+}
---
+++ backend/src/test/java/com/dwelzy/dwelzy/DwelzyApplicationTests.java
@@ -0,0 +1,13 @@
+package com.dwelzy.dwelzy;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DwelzyApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
